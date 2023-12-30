20d713f54e5818d1a41cdce2035e331d43b7adbc
VeronyMark <veronika.markova2002@gmail.com>
2023-12-30 04:01:40 +0100

    laravel zmena welcome page -> nevyriesene delenie a prekliky na konkretne blogy

--- routes/web.php
+++ routes/web.php
@@ -3,6 +3,11 @@
 use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 
+
+use App\Models\Post;
+use App\Models\Category;
+use App\Models\User;
+
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -14,8 +19,12 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
+
+
 Route::get('/', function () {
-    return view('welcome');
+    return view('welcome', [
+    'posts' => Post::latest()->with('category','author') ->get()
+    ]);
 });
 
 Route::get('/dashboard', function () {
